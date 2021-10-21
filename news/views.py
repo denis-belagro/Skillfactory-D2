24eb305bd6293d9a1dc8f85fb814747a02809b70
Denis <dzianis.shcherba@gmail.com>
2021-10-21 23:09:10 +0300

    D4.4 final completed

--- news/views.py
+++ news/views.py
@@ -6,26 +6,14 @@ from .models import Post
 from .filters import ProductFilter # импортируем недавно написанный фильтр
 from .forms import ProductForm # импортируем нашу форму
 
- 
-"""
-class ProductsList(ListView):
-    model = Post  # указываем модель, объекты которой мы будем выводить
-    template_name = 'newspage.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
-    context_object_name = 'newspage'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
-    queryset = Post.objects.order_by('-dateCreation')
-"""
+
 class NewsList(ListView):
     model = Post
     template_name = 'newspage.html'
     context_object_name = 'newspage'
     ordering = ['-dateCreation']
     paginate_by = 10 # поставим постраничный вывод в один элемент
-    """
-    def get_context_data(self, **kwargs): # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет, полиморфизм, мы скучали!!!)
-        context = super().get_context_data(**kwargs)
-        context['filter'] = ProductFilter(self.request.GET, queryset=self.get_queryset()) # вписываем наш фильтр в контекст
-        return context
-    """
+ 
     def get_filter(self):
         return ProductFilter(self.request.GET, queryset=super().get_queryset())
     def get_queryset(self):
@@ -38,12 +26,7 @@ class NewsList(ListView):
     
 
 # создаём представление, в котором будут детали конкретного отдельного товара
-"""
-class ProductDetail(DetailView):
-    model = Post # модель всё та же, но мы хотим получать детали конкретно отдельного товара
-    template_name = 'newsonly.html' # название шаблона будет product.html
-    context_object_name = 'newsonly' # название объекта  
-"""
+
 class NewsDetailView(DetailView):
     template_name = 'news_detail.html'
     queryset = Post.objects.all()
